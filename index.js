589312ae195d0e31ef4d05ff54338aca688c74f2
jeevanreddyb <41283608+jeevanreddyb@users.noreply.github.com>
2020-03-21 18:59:14 +0530

    chat

---
+++ index.js
@@ -0,0 +1,33 @@
+var express=require('express');
+var socket=require('socket.io');
+const port =  process.env.PORT || 3000;
+//App setup
+var app=express();
+var server=app.listen(port,function()
+{
+    console.log(`Listening on Port ${port}`);
+});
+
+//Static files
+
+app.use(express.static('public'));
+
+//Socket Setup
+
+var io=socket(server);
+
+io.on('connection',function(socket){
+    console.log("Connected to socket");
+
+    socket.on('chat',function(data){
+        
+        io.sockets.emit('chat',data);
+    });
+
+    socket.on('typing',function(data){
+        socket.broadcast.emit('typing',data);
+    })
+});
+
+
+
